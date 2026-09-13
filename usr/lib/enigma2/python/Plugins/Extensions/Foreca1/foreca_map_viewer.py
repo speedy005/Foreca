7a67c1693973c7ab6d10b00f0e5a6a3beb41c18f
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 16:50:54 +0200

    Update foreca_map_viewer.py

--- usr/lib/enigma2/python/Plugins/Extensions/Foreca1/foreca_map_viewer.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Foreca1/foreca_map_viewer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Copyright (c) @Lululla 2026
+# Copyright (c) @speedy005 2026
 # foreca_map_viewer.py - Foreca map viewer with OSM background and Foreca
 # tiles overlay
 
