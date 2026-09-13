20e455bc2a294a13a0d2b1e894241eb8de3744cf
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 16:52:49 +0200

    Update hour_detail.py

--- usr/lib/enigma2/python/Plugins/Extensions/Foreca1/hour_detail.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Foreca1/hour_detail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Copyright (c) @Lululla 2026
+# Copyright (c) @speedy005 2026
 # hour_detail.py - Detailed view for a single hour with animated icons
 
 import glob
