c2fc65201e6c11ef5ce196c85a8b28af4fd86153
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 16:50:37 +0200

    Update foreca_map_menu.py

--- usr/lib/enigma2/python/Plugins/Extensions/Foreca1/foreca_map_menu.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Foreca1/foreca_map_menu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Copyright (c) @Lululla 2026
+# Copyright (c) @speedy005 2026
 # foreca_map_menu.py - Foreca One map selection menu
 
 from Screens.Screen import Screen
