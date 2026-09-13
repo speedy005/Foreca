eff1b3b3921003d4004735b1842395066f6a48df
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 16:53:09 +0200

    Modify copyright and author information in info_dialog.py
    Updated copyright information and author details in info_dialog.py.

--- usr/lib/enigma2/python/Plugins/Extensions/Foreca1/info_dialog.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Foreca1/info_dialog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Copyright (c) @Lululla 2026
+# Copyright (c) @speedy005 2026
 # info_dialog.py - Info about the plugin
 
 from Screens.Screen import Screen
