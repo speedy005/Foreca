896d721a50145e443423a27e475ddd97a31f9e1c
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 16:48:20 +0200

    Update copyright notice in MoonPhase.py

--- usr/lib/enigma2/python/Plugins/Extensions/Foreca1/MoonPhase.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Foreca1/MoonPhase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Copyright (c) @Lululla 2026
+# Copyright (c) @speedy005 2026
 # MoonPhase.py - Accurate lunar phase calculations (Meeus algorithms)
 
 import glob
