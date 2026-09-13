162187479e813fd9d6869f6baa7ffa775d844863
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 16:55:21 +0200

    Update translation_setup.py

--- usr/lib/enigma2/python/Plugins/Extensions/Foreca1/translation_setup.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Foreca1/translation_setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Copyright (c) @Lululla 2026
+# Copyright (c) @speedy005 2026
 # translation_setup.py - Translation settings screen
 
 from Screens.Screen import Screen
