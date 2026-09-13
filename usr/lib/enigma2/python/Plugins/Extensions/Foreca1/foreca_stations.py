6a0ddcfabd4e27fc9d59a51dccb4cf6f9e541c6c
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 16:51:34 +0200

    Update foreca_stations.py

--- usr/lib/enigma2/python/Plugins/Extensions/Foreca1/foreca_stations.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Foreca1/foreca_stations.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Copyright (c) @Lululla 2026
+# Copyright (c) @speedy005 2026
 # foreca_stations.py - Display nearby station observations
 
 from Screens.Screen import Screen
