b898ce55642b7cda9ca439dcc680e65d3115a3ec
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-18 21:38:58 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 # ForecaOne Installer
 # =========================================================
 
-version='1.5.9'
+version='1.6.0'
 changelog='Fix Malformed Locale Language. Offer coffee if you like this plugin'
 
 
